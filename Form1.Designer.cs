ff63f59aee2b53fccabc96135a6a26420f8cb5b9
Артём Соломатов <artemka2004sol@gmail.com>
2023-12-05 15:39:07 +0500

    #1 chart is done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.txtXmin = new System.Windows.Forms.TextBox();
+            this.txtXmax = new System.Windows.Forms.TextBox();
+            this.txtStep = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 4;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "y1";
+            series2.BorderWidth = 4;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "y2";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(688, 379);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.AutoSize = true;
+            this.btnCalc.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnCalc.Location = new System.Drawing.Point(373, 401);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(60, 23);
+            this.btnCalc.TabIndex = 1;
+            this.btnCalc.Text = "calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // txtXmin
+            // 
+            this.txtXmin.Location = new System.Drawing.Point(53, 403);
+            this.txtXmin.Name = "txtXmin";
+            this.txtXmin.Size = new System.Drawing.Size(66, 20);
+            this.txtXmin.TabIndex = 2;
+            // 
+            // txtXmax
+            // 
+            this.txtXmax.Location = new System.Drawing.Point(176, 403);
+            this.txtXmax.Name = "txtXmax";
+            this.txtXmax.Size = new System.Drawing.Size(66, 20);
+            this.txtXmax.TabIndex = 3;
+            // 
+            // txtStep
+            // 
+            this.txtStep.Location = new System.Drawing.Point(301, 403);
+            this.txtStep.Name = "txtStep";
+            this.txtStep.Size = new System.Drawing.Size(66, 20);
+            this.txtStep.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 406);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Xmin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 406);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Xmax";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 406);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "step";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(510, 457);
+            this.ClientSize = new System.Drawing.Size(712, 457);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStep);
+            this.Controls.Add(this.txtXmax);
+            this.Controls.Add(this.txtXmin);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.TextBox txtXmin;
+        private System.Windows.Forms.TextBox txtXmax;
+        private System.Windows.Forms.TextBox txtStep;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,46 @@ namespace EducationalPractice1
         {
             InitializeComponent();
         }
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtXmin.Text, out double Xmin) &&
+            double.TryParse(txtXmax.Text, out double Xmax) &&
+            double.TryParse(txtStep.Text, out double step))
+            {
+                // узнаём количество точек графика
+                int count = (int)Math.Ceiling((Xmax - Xmin) / step) + 1;
+                // массивы точек
+                double[] x = new double[count];
+                double[] y1 = new double[count];
+                double[] y2 = new double[count];
+                // переменная для вычислений
+                var d = 1.3;
+                // цикл для выставления границ под точки
+                for (int i = 0; i < count; i++)
+                {
+                    // формулы вычислений
+                    x[i] = Xmin + step * i;
+                    y1[i] = Math.Pow(x[i], 4) + Math.Cos(2 + (Math.Pow(x[i], 3)) - d);
+                    y2[i] = Math.Cos(Math.Pow(x[i], 2));
+                }
+                // настраиваем оси графика
+                chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+                chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+                // настраиваем шаг сетки
+                chart1.ChartAreas[0].AxisX.MajorGrid.Interval = step;
+                // задаём точки на графике
+                chart1.Series[0].Points.DataBindXY(x, y1);
+                chart1.Series[1].Points.DataBindXY(x, y2);
+            }
+            else
+            {
+                MessageBox.Show("check the entered data");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
