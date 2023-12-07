27323b0dcb0dabfbdf3b92816a04c33c4cae9ccf
TeBe XaNa TrAtAtA <mrnickone@bk.ru>
2023-12-07 20:45:02 +0500

    #3 gun done!

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btnCalc = new System.Windows.Forms.Button();
             this.txtXmin = new System.Windows.Forms.TextBox();
@@ -40,29 +40,33 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(12, 12);
             this.chart1.Name = "chart1";
-            series1.BorderWidth = 4;
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series1.Legend = "Legend1";
-            series1.Name = "y1";
-            series2.BorderWidth = 4;
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series2.Legend = "Legend1";
-            series2.Name = "y2";
-            this.chart1.Series.Add(series1);
-            this.chart1.Series.Add(series2);
+            series3.BorderWidth = 4;
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "y1";
+            series4.BorderWidth = 4;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.Legend = "Legend1";
+            series4.Name = "y2";
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
             this.chart1.Size = new System.Drawing.Size(688, 379);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -71,7 +75,7 @@
             // 
             this.btnCalc.AutoSize = true;
             this.btnCalc.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.btnCalc.Location = new System.Drawing.Point(373, 401);
+            this.btnCalc.Location = new System.Drawing.Point(640, 401);
             this.btnCalc.Name = "btnCalc";
             this.btnCalc.Size = new System.Drawing.Size(60, 23);
             this.btnCalc.TabIndex = 1;
@@ -81,21 +85,21 @@
             // 
             // txtXmin
             // 
-            this.txtXmin.Location = new System.Drawing.Point(53, 403);
+            this.txtXmin.Location = new System.Drawing.Point(48, 403);
             this.txtXmin.Name = "txtXmin";
             this.txtXmin.Size = new System.Drawing.Size(66, 20);
             this.txtXmin.TabIndex = 2;
             // 
             // txtXmax
             // 
-            this.txtXmax.Location = new System.Drawing.Point(176, 403);
+            this.txtXmax.Location = new System.Drawing.Point(160, 403);
             this.txtXmax.Name = "txtXmax";
             this.txtXmax.Size = new System.Drawing.Size(66, 20);
             this.txtXmax.TabIndex = 3;
             // 
             // txtStep
             // 
-            this.txtStep.Location = new System.Drawing.Point(301, 403);
+            this.txtStep.Location = new System.Drawing.Point(266, 403);
             this.txtStep.Name = "txtStep";
             this.txtStep.Size = new System.Drawing.Size(66, 20);
             this.txtStep.TabIndex = 4;
@@ -112,7 +116,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(135, 406);
+            this.label2.Location = new System.Drawing.Point(121, 406);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 6;
@@ -121,17 +125,53 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(260, 406);
+            this.label3.Location = new System.Drawing.Point(233, 406);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(27, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "step";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(339, 406);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "step";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(372, 403);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(66, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(442, 406);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "step";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(475, 403);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(66, 20);
+            this.textBox2.TabIndex = 10;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(712, 457);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -158,6 +198,10 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -26,17 +26,16 @@ namespace EducationalPractice1
                 int count = (int)Math.Ceiling((Xmax - Xmin) / step) + 1;
                 // массивы точек
                 double[] x = new double[count];
-                double[] y1 = new double[count];
+                double[] y = new double[count];
                 // переменная для вычислений
-                var y = 5;
-                var z = 5;
-                var c = 5;
+                double y1 = 5;
+                double c = 5;
                 // цикл для выставления границ под точки
                 for (int i = 0; i < count; i++)
                 {
                     // формулы вычислений
                     x[i] = Xmin + step * i;
-                    y1[i] = (Math.Log(Math.Pow(y, - Math.Sqrt(Math.Abs(z)))) * (z - y / 2) + Math.Pow(Math.Sin(c), 2) * Math.Atan(c));
+                    y[i] = (Math.Log(Math.Pow(y1, -Math.Sqrt(Math.Abs(x[i])))) * (x[i] - y1 / 2) + Math.Pow(Math.Sin(c), 2) * Math.Atan(c));
                 }
                 // настраиваем оси графика
                 chart1.ChartAreas[0].AxisX.Minimum = Xmin;
@@ -44,7 +43,7 @@ namespace EducationalPractice1
                 // настраиваем шаг сетки
                 chart1.ChartAreas[0].AxisX.MajorGrid.Interval = step;
                 // задаём точки на графике
-                chart1.Series[0].Points.DataBindXY(x, y1);
+                chart1.Series[0].Points.DataBindXY(x, y);
             }
             else
             {
